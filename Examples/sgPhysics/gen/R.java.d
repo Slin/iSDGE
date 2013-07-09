a921f7ea5e8896389214fff1bb3f28fc1cc66842
Nils Daumann <nils_daumann@slindev.com>
2013-07-09 13:41:30 +0200

    deleted android auto generated files

--- Examples/sgPhysics/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-/Users/nils_daumann/Documents/Dev/iSDGE/Examples/sgPhysics/gen/com/android/isdge/R.java \
- : /Users/nils_daumann/Documents/Dev/iSDGE/Examples/sgPhysics/res/values/strings.xml \
-/Users/nils_daumann/Documents/Dev/iSDGE/Examples/sgPhysics/bin/AndroidManifest.xml \
--- Examples/sgPhysics/gen/com/android/isdge/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.android.isdge;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
